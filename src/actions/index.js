4c7be7773b4e55826e7ff609c252bd6ab375b788
RutSzymon <rut.szymon@gmail.com>
2020-06-30 13:10:55 +0200

    163. Making a Request From an Action Creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,10 @@
-export const fetchPosts = () => {
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPosts = async () => {
+  await response = jsonPlaceholder.get("/posts");
+
   return {
     type: "FETCH_POSTS",
+    payload: response,
   };
 };
